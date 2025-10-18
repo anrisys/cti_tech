10ccec4a41c91cd8bd81dc297cdb02cca453a56b
anrisys <anris.y.simorangkir@gmail.com>
2025-10-18 16:33:50 +0700

    FEAT: Add update task API + introduce base-task DTO

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,41 +1,67 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { CreateTaskDTO, TaskStatus } from './dto/create-task.dto';
-import { ResponseCode, ResponseMessage } from 'src/common/decorators/api-response.decorator';
-import { UpdateTaskStatuskDTO } from './dto/update-task-status.dto';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import {
+  ResponseCode,
+  ResponseMessage,
+} from 'src/common/decorators/api-response.decorator';
+import { UpdateTaskStatusDTO } from './dto/update-task-status.dto';
+import { UpdateTaskDTO } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) {}
 
-    @Post()
-    @ResponseMessage('Task created successfully')
-    @ResponseCode('TASK_CREATED')
-    create(@Body() task: CreateTaskDTO) {
-        return this.tasksService.create(task);
-    }
+  @Post()
+  @ResponseMessage('Task created successfully')
+  @ResponseCode('TASK_CREATED')
+  create(@Body() task: CreateTaskDTO) {
+    return this.tasksService.create(task);
+  }
 
-    @Get()
-    @ResponseMessage('Tasks retrieved successfully')
-    findAll() {
-        return this.tasksService.findAll();
-    }
+  @Get()
+  @ResponseMessage('Tasks retrieved successfully')
+  findAll() {
+    return this.tasksService.findAll();
+  }
 
-    @Patch(':id/status')
-    @ResponseMessage('Task status updated successfully')
-    @ResponseCode('TASK_UPDATED')
-    updateStatus(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() newTaskStatus: UpdateTaskStatuskDTO,
-    ) {
-        return this.tasksService.updateStatus(id, newTaskStatus.status);
-    }
+  @Patch(':id/status')
+  @ResponseMessage('Task status updated successfully')
+  @ResponseCode('TASK_UPDATED')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() newTaskStatus: UpdateTaskStatusDTO,
+  ) {
+    return this.tasksService.updateStatus(id, newTaskStatus.status);
+  }
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @ResponseMessage('Task deleted successfully')
-    @ResponseCode('TASK_DELETED')
-    remove(@Param('id', ParseIntPipe) id: number) {
-        return this.tasksService.remove(id);
-    }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ResponseMessage('Task deleted successfully')
+  @ResponseCode('TASK_DELETED')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.remove(id);
+  }
+
+  @Put(':id')
+  @ResponseCode('TASK_UPDATED')
+  @ResponseMessage('Task updated successfully')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() task: UpdateTaskDTO,
+  ) {
+    return this.tasksService.updateTask(id, task);
+  }
 }
